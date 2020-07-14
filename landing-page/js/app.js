a6253d157d81172c62ec9f9f14f0f95bf21a0769
George Kim <jungwhan.kim@mckinleyrice.com>
2020-07-14 19:27:05 +0900

    Add scroll-interactive navbar and anchor tag

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,36 +1,66 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+const containerCount = document.querySelectorAll(".landing__container").length;
+console.log(containerCount);
+for (let i = 1; i <= containerCount; i++) {
+  let targetItem = document.getElementById("section" + i);
+  console.log(targetItem);
+  let bounding = targetItem.getBoundingClientRect();
+  console.log(bounding);
 
-/**
- * Define Global Variables
- *
- */
+  targetItem.addEventListener("click", () => {
+    targetItem.scrollIntoView({ behavior: "smooth" });
+  });
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+const navLinkCount = document.getElementsByTagName("li").length;
+console.log(navLinkCount);
+for (let i = 1; i <= navLinkCount; i++) {
+  let targetLink = [...document.getElementsByClassName("section" + i)];
+  console.log(targetLink);
+  targetLink.forEach((el) => {
+    el.addEventListener("click", () => {
+      let targetItem = document.getElementById("section" + i);
+      targetItem.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+// function that makes the navigation bar shrink in height once the user has scrolled down for more than x pixels
+window.onscroll = () => shrinkNav();
 
-// build the nav
+shrinkNav = () => {
+  if (
+    document.documentElement.scrollTop > 100 ||
+    document.body.scrollTop > 100
+  ) {
+    // shrink nav bar height
+    document.querySelector(".navbar").style.padding = "10px 0";
+    // shrink logo
+    document.querySelector(".logo-img").style.transform = "scale(0.6)";
+    document.querySelector(".page-title").style.fontSize = "18px";
+  } else {
+    document.querySelector(".navbar").style.padding = "20px 0";
+    document.querySelector(".logo-img").style.transform = "scale(0.8)";
+    document.querySelector(".page-title").style.fontSize = "24px";
+  }
+};
+
+// function for the scroll progress bar located under the navigation bar
+document.addEventListener(
+  "scroll",
+  () => {
+    let scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    let scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = (scrollTop / scrollBottom) * 100 + "%";
+
+    document
+      .querySelector(".progress")
+      .style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
 
 // Add class 'active' to section when near top of viewport
 
